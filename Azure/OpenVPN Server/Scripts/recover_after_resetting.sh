bcb50580a51b36a1fef4ef5f40d8bf1b5c54e39f
sunsh9876543210 <82513690+sunsh9876543210@users.noreply.github.com>
2022-05-25 22:38:20 +0900

    Create recover_after_resetting.sh

---
+++ Azure/OpenVPN Server/Scripts/recover_after_resetting.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install openvpn
+wget -P ~/ https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz
+tar xvf EasyRSA-3.0.8.tgz
+#not completed
