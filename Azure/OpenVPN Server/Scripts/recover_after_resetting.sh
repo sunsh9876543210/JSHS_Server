a5384a6a681606e9ca5ab0e8680daff0bdf61dae
sunsh9876543210 <82513690+sunsh9876543210@users.noreply.github.com>
2022-06-28 20:47:17 +0900

    Update recover_after_resetting.sh

--- Azure/OpenVPN Server/Scripts/recover_after_resetting.sh
+++ Azure/OpenVPN Server/Scripts/recover_after_resetting.sh
@@ -1,6 +1,8 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install openvpn
+EasyRSA_PATH="~/EasyRSA-3.0.8"
 wget -P ~/ https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz
 tar xvf EasyRSA-3.0.8.tgz
-#not completed
+sudo cp ./assets/vars ${EasyRSA_PATH}/vars
+${EasyRSA_PATH}/easyrsa init-pki
